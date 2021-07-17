c100a2d405c4fe7164a74fe25ea57330bde4429d
Shaun Kent <shaunkent81@gmail.com>
2021-07-17 14:36:24 -0700

    add veggie and protien choice arrays

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,15 @@ function Pizza (toppings, size) {
 }
 
 Pizza.prototype.costCalculator = function() {
-  if(this.toppings === "artichoke") {
-    return this.basePrice = 11;
+  let veggieChoices = ["artichoke", "olives", "mushrooms", "basil"]
+  let protienChoices = ["pepperoni", "bacon", "ham", "sausage"]
+  if(veggieChoices.includes(this.toppings)) {
+    this.basePrice += 1;
+  }else if(protienChoices.includes(this.toppings)) {
+    this.basePrice += 2;
   }
+  return this.basePrice;
 }
 
-let pizza1 = new Pizza("artichoke", "medium")
+let pizza1 = new Pizza("ham", "medium")
+pizza1.costCalculator();
